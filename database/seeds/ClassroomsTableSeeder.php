5ec92fbe7180dd2e3af5b3015df560f643f50726
Luigi <luigismilzo@live.com>
2020-06-19 16:22:30 +0200

    Classrooms table seeded

--- database/seeds/ClassroomsTableSeeder.php
+++ database/seeds/ClassroomsTableSeeder.php
@@ -13,6 +13,13 @@ class ClassroomsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-      
+      for ($i = 0; $i < 20; $i++)
+      {
+          $newClassroom = new Classroom();
+          $newClassroom->name = $faker->lastName();
+          $newClassroom->partecipants = $faker->numberBetween(15, 30);
+
+          $newClassroom->save();
+      }
     }
 }
